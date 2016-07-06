7e3abce93b8850b6a566078c5b2a32eaa9951820
kimjongchan <nanch@naver.com>
2016-07-06 14:53:06 +0900

    종찬 : 회원가입 && includes 폴더 추가

---
+++ register_page.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<html>
+<body>
+<div>
+<h1>회원 가입</h1>
+
+<form action="register_process.php" method="post">
+<table>
+<tr>
+<td>ID</td><td><input type="text" name="id"></td>
+</tr>
+<tr>
+<td>password</td><td><input type="text" name="password"></td>
+</tr>
+<tr>
+<td><input type="submit" value="회원 가입"></td>
+</tr>
+</table>
+</form>
+
+<?php
+
+
+
+
+?>
