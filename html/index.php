d8f474f97638086de24c356d6e45b0cc6e51f9ac
kimjongchan <nanch@naver.com>
2016-07-07 11:11:36 +0900

    종찬 : 로그인시 게임참가 버튼 활성화

--- html/index.php
+++ html/index.php
@@ -14,49 +14,54 @@
 
 <div id="wrap">
 	<?php include 'header.php'; ?>
-	<div id="content">
-		<div id="content_l">
-			<form>
-				<input type="button" name="start_game" value="게임 참가하기">
-			</form>
-		</div>
-		<div id="content_r">
-			<div id="login">
-			<?php 
-				if(check_login()){
-					echo '<table>';
-					echo '<td>'.$_GET['id'].'님 환영합니다.</td>';
+	<div id="content">							
+		<?php 
+			if(check_login()){
+				echo '<table>';
+				echo '<tr>';
+				echo '<td>'.$_GET['id'].'님 환영합니다.</td>';
+				echo '</tr>';
 					
-			?>
-					<td><form action="logout.php" method="get">
-					<input type="submit" value="로그아웃"></td>
-					</form></td>
-					</tr>
-					</table>
+		?>
+				<div id="content_l">
+					<form>
+						<input type="button" name="start_game" value="게임 참가하기">
+					</form>
+				</div>
+				<div id="content_r">
+					<div id="login">
+						<tr>
+							<td><form action="logout.php" method="get">
+							<input type="submit" value="로그아웃"></td>
+							</form></td>
+						</tr>
+						</table>
 					</div>
-			<?php		
-				} else {				
-			?>					
-				<form action="login_process.php" method="post">
-					<table>
-						<tr>
-							<th>ID</th>
-							<td><input type="text" name="id"></td>
-						</tr>
-						<tr>
-							<th>PW</th>
-							<td><input type="password" name="password"></td>
-						</tr>
-					</table>
-					<td rowspan="2"><input type="submit" name="login_btn" value="로그인"></td>
-				</form>
-
-				<form action="register_page.php" method="get">
-					<input type="submit" value="회원가입">
-				</form>
-				<?php } ?>
-			</div>
-		</div>
+				</div>	
+		<?php		
+			} else {				
+		?>		
+				<div id="content_r">
+					<div id="login">
+						<form action="login_process.php" method="post">
+							<table>
+								<tr>
+									<th>ID</th>
+									<td><input type="text" name="id"></td>
+								</tr>
+								<tr>
+									<th>PW</th>
+									<td><input type="password" name="password"></td>
+								</tr>
+							</table>
+							<td rowspan="2"><input type="submit" name="login_btn" value="로그인"></td>
+						</form>
+						<form action="register_page.php" method="get">
+							<input type="submit" value="회원가입">
+						</form>
+					</div>
+				</div>
+		<?php } ?>		
 	</div>
 </div>
 <?php include 'footer.php'; ?>
