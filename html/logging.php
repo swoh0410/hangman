eb8d739fe946977348ed0b8404b5387ba8831bf1
Calvin <calvin.caulfield@gmail.com>
2016-09-06 14:41:28 +0900

    로그파일 작업 실패시 즉시 알려주도록 함. (서버에서 파일 권한 때문에 정상 작동 안하고 있었음)

--- html/logging.php
+++ html/logging.php
@@ -1,11 +1,14 @@
 <?php
-
-
-
 function debug_log($log) {
 	$file_name = 'log.txt';
 	$handle = fopen($file_name, 'a');
-	fwrite($handle, $log."\n");
+	if ($handle === false) {
+		die('dedub_log fopen error!');
+	}
+	$bytes_written = fwrite($handle, $log."\n");
+	if ($bytes_written === false) {
+		die('dedub_log fwrite error!');
+	}
 	fclose($handle);
 }
 ?>
